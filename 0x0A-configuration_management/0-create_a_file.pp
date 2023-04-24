3097171b376682c31f9eedeaafe4fa9d75ead7e2
Ohyeenlorlar <oyinlolaelizabeth2017@gmail.com>
2023-04-24 08:14:00 -0700

    for this task

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,10 @@
-I love Puppet
+# Create a file in /tmp
+
+file { 'holberton':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/holberton',
+}
